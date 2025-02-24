eed0305b99aa1e8ab7b0e4012f774ef616f4d916
Rui Li <rli3@caltech.edu>
2025-02-24 00:11:36 -0800

    add benchmarking script

---
+++ examples/multi_grid/benchmark.py
@@ -0,0 +1,29 @@
+from ase.build import bulk
+from pyscf.pbc.tools.pyscf_ase import ase_atoms_to_pyscf
+import numpy as np
+from pyscf.pbc import gto, tools
+from gpu4pyscf.pbc import dft as pbcdft
+from gpu4pyscf.pbc.dft import multi_grid
+
+
+diamond_cell = bulk('C', 'sc', a=4)
+
+lattice_vectors = diamond_cell.cell
+cell = gto.M(
+    h=np.array(lattice_vectors),
+    atom=ase_atoms_to_pyscf(bulk('C', 'sc', a=4)),
+    basis='minao',
+    verbose=6,
+    unit='aa',
+    ke_cutoff=200
+)
+cell.exp_to_discard = 0.1
+
+cell = tools.super_cell(cell, [2, 2, 2])
+
+mf = pbcdft.RKS(cell)
+mf.xc = "LDA"
+mf.max_cycle = 0
+mf = multi_grid.fftdf(mf)
+mf.with_df.ngrids = 4  # number of sets of grid points
+mf.kernel()
