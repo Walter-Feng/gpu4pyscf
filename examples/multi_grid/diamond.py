f7e7c384af1586968fc66fe9972f4db38cd1ad34
Rui Li <rli3@caltech.edu>
2025-04-03 13:48:15 -0700

    update diamond script

--- examples/multi_grid/diamond.py
+++ examples/multi_grid/diamond.py
@@ -2,78 +2,104 @@ from ase.build import bulk
 from pyscf.pbc.tools.pyscf_ase import ase_atoms_to_pyscf
 import numpy as np
 from pyscf.pbc import gto, tools
-from pyscf.pbc import dft
+from pyscf.pbc import dft as cpu_pbcdft
 from pyscf.pbc.dft import multigrid as cpu_multi_grid
 
 from gpu4pyscf.pbc import dft as gpu_pbcdft
 from gpu4pyscf.pbc.dft import multi_grid as gpu_multi_grid_mine
-from gpu4pyscf.pbc.dft import multi_grid_store_ao as gpu_multi_grid_ao
 from gpu4pyscf.pbc.dft import multigrid as gpu_multi_grid_qiming
 
-cell=gto.Cell()
 
-import cupy.cuda
-# cupy.cuda.set_allocator(cupy.cuda.MemoryPool(cupy.cuda.malloc_managed).malloc)
-boxlen=12
-diamond_cell = bulk('C', 'diamond', orthorhombic=True)
+def make_diamond_cell(ke_cutoff, precision, super_cell):
+    diamond_cell = bulk("C", "diamond", orthorhombic=True)
 
-lattice_vectors = diamond_cell.cell
-cell = gto.M(
-    h=np.array(diamond_cell.cell),
-    atom=ase_atoms_to_pyscf(diamond_cell),
-    basis='gth-tzv2p',
-    verbose=5,
-    unit='aa',
-    ke_cutoff=200,
-    pseudo = 'gth-pade'
-)
-cell.exp_to_discard = 0.1
+    cell = gto.M(
+        h=np.array(diamond_cell.cell),
+        atom=ase_atoms_to_pyscf(diamond_cell),
+        basis="gth-tzv2p",
+        verbose=6,
+        unit="aa",
+        ke_cutoff=ke_cutoff,
+        precision=precision,
+        pseudo="gth-pade",
+    )
+    cell.exp_to_discard = 0.1
 
-cell = tools.super_cell(cell, [1, 1, 1])
-cell.build()
-
-print("="*100)
-print("gpu_multi_grid_mine")
-print("="*100)
-gpu_mf = gpu_pbcdft.RKS(cell)
-gpu_mf.xc = "LDA"
-gpu_mf.init_guess = 'atom' # atom guess is fast
-gpu_mf.max_cycle = 10
-gpu_mf = gpu_multi_grid_mine.fftdf(gpu_mf)
-gpu_mf.with_df.ngrids = 4
-gpu_mf.kernel()
-
-print("="*100)
-print("gpu_multi_grid_qiming")
-print("="*100)
-gpu_mf = gpu_pbcdft.RKS(cell)
-gpu_mf.xc = "LDA"
-gpu_mf.init_guess = 'atom'
-gpu_mf.max_cycle = 10
-gpu_mf._numint = gpu_multi_grid_qiming.MultiGridNumInt(cell)
-gpu_mf.kernel()
-
-print("="*100)
-print("cpu_multi_grid")
-print("="*100)  
-mf=dft.RKS(cell)
-mf.xc = "LDA"
-# mf.xc = "PBE,PBE"
-mf.max_cycle = 10
-# mf.init_guess = 'atom' # atom guess is fast
-mf.with_df = cpu_multi_grid.MultiGridFFTDF(cell)
-mf.with_df.ngrids = 4 # number of sets of grid points
-mf.kernel()
-
-print("="*100)
-print("gpu_multi_grid_ao")
-print("="*100)
-gpu_mf = gpu_pbcdft.RKS(cell)
-gpu_mf.xc = "LDA"
-gpu_mf.init_guess = 'atom' # atom guess is fast
-gpu_mf.max_cycle = 10
-gpu_mf = gpu_multi_grid_ao.fftdf(gpu_mf)
-gpu_mf.with_df.ngrids = 4
-gpu_mf.kernel()
+    cell = tools.super_cell(cell, super_cell)
+    cell.build()
+    return cell
 
 
+def make_multi_grid_object(mf, version):
+    if version == "Rui":
+        return gpu_multi_grid_mine.fftdf(mf)
+    elif version == "Qiming":
+        mf._numint = gpu_multi_grid_qiming.MultiGridNumInt(mf.cell)
+        return mf
+    elif version == "Qiming_CPU":
+        mf.with_df = cpu_multi_grid.MultiGridFFTDF(mf.cell)
+        return mf
+    elif version == "Xing_CPU":
+        mf.with_df = cpu_multi_grid.MultiGridFFTDF2(mf.cell)
+        return mf
+
+    raise ValueError(f"Unknown version: {version}")
+
+
+def make_mf(cell, xc, version):
+    mf = None
+    if "CPU" in version:
+        mf = cpu_pbcdft.RKS(cell)
+    else:
+        mf = gpu_pbcdft.RKS(cell)
+    mf.xc = xc
+    mf.init_guess = "atom"
+    mf = make_multi_grid_object(mf, version)
+    return mf
+
+
+def print_snipping_line():
+    print()
+    print("-" * 30, "snip me", "-" * 30)
+    print()
+
+
+def print_header(name):
+    print("=" * 100)
+    print(name)
+    print("=" * 100)
+
+
+def run(ke_cutoff, precision, super_cell, xc, version):
+    print_snipping_line()
+    print_header(
+        f"ke_cutoff={ke_cutoff}, precision={precision}, super_cell={super_cell}, xc={xc}, version={version}"
+    )
+    cell = make_diamond_cell(ke_cutoff, precision, super_cell)
+    mf = make_mf(cell, xc, version)
+    mf.kernel()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Run DFT calculations on diamond structure"
+    )
+    parser.add_argument(
+        "platform",
+        choices=["cpu", "gpu"],
+        help="Choose between CPU or GPU implementation",
+    )
+    args = parser.parse_args()
+
+    if args.platform == "cpu":
+        versions = ["Qiming_CPU", "Xing_CPU"]
+    else:
+        versions = ["Rui", "Qiming"]
+    for super_cell in [(1, 1, 1), (2, 2, 2), (3, 3, 3), (4, 4, 4)]:
+        for precision in [1e-6, 1e-8, 1e-10]:
+            for ke_cutoff in [100, 150, 200]:
+                for version in versions:
+                    for xc in ["LDA", "PBE"]:
+                        run(ke_cutoff, precision, super_cell, xc, version)
