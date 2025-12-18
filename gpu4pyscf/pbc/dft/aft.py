898c45ce54626cf9d7da77b9b261d2196bd08e27
Rui Li <walterfeng@foxmail.com>
2025-12-18 15:13:16 -0800

    savepoint for getting correct primitive pair in real space

--- gpu4pyscf/pbc/dft/aft.py
+++ gpu4pyscf/pbc/dft/aft.py
@@ -34,6 +34,52 @@ import gpu4pyscf.pbc.dft.gen_grid as gen_grid
 from gpu4pyscf.lib.cupy_helper import contract, tag_array, load_library
 
 
+def common_fac_sp(angular: int):
+    match angular:
+        case 0:
+            return 0.282094791773878143
+        case 1:
+            return 0.488602511902919921
+        case _:
+            return 1.0
+
+
+def polynomials(coords: cp.ndarray, angular: int):
+    assert coords.shape[-1] == 3
+    x, y, z = coords.T
+    n_functions = (angular + 1) * (angular + 2) // 2
+
+    result = cp.ones((n_functions, len(x)))
+
+    match angular:
+        case 0:
+            pass
+        case 1:
+            result[0] = x
+            result[1] = y
+            result[2] = z
+        case 2:
+            result[0] = x * x
+            result[1] = x * y
+            result[2] = x * z
+            result[3] = y * y
+            result[4] = y * z
+            result[5] = z * z
+        case _:
+            raise NotImplementedError
+
+    return result * common_fac_sp(angular)
+
+
+def power_series(norm_coords: cp.ndarray, max_power: int):
+    result = cp.zeros((max_power + 1, len(norm_coords)))
+    result[0] = norm_coords
+    for i in range(max_power):
+        result[i + 1] = result[i] * norm_coords
+
+    return result
+
+
 class AFTDFNumInt(pbc_numint.NumInt):
     def __init__(self, cell: gto.Cell):
         self.cell = cell
@@ -56,11 +102,15 @@ class AFTDFNumInt(pbc_numint.NumInt):
 
         self.primitive = cell
         self.transform_coeff = sorted_transform_coeff
-        self.shell_to_ao = cp.asarray(make_loc(cell._bas, 'cart'), dtype=cp.int32)
+        self.shell_to_ao = np.asarray(make_loc(cell._bas, 'cart'), dtype=cp.int32)
+        self.n_primitives = len(cell._bas)
         self.n_primitive_functions = transform_shape[0]
         self.n_functions = transform_shape[1]
 
+        self.Gv = cp.asarray(cell.get_Gv())
+
         self.primitive_values = pbc_numint.eval_ao(cell, self.grid.coords)
+        self.neighboring_images = neighboring_images
 
         exponents = cell._env[cell._bas[:, PTR_EXP]]
         angulars = cell._bas[:, ANG_OF]
@@ -78,6 +128,45 @@ class AFTDFNumInt(pbc_numint.NumInt):
         pass
         # vj = get_j_kpts(self, dm, hermi, kpts, kpts_band)
         # return vj
+
+    def get_primitive_pair_values_in_reciprocal(self, i_primitive, j_primitive):
+        primitive_pair = [i_primitive, j_primitive]
+        real_coords = cp.asarray(self.grid.coords)
+        cell = self.primitive
+
+        i_function_indices = np.arange(self.shell_to_ao[i_primitive], self.shell_to_ao[i_primitive + 1])
+        j_function_indices = np.arange(self.shell_to_ao[j_primitive], self.shell_to_ao[j_primitive + 1])
+
+        i_atom_index, j_atom_index = cell._bas[primitive_pair, ATOM_OF]
+        i_angular, j_angular = cell._bas[primitive_pair, ANG_OF]
+
+        i_exponent, j_exponent = cell._env[cell._bas[primitive_pair, PTR_EXP]]
+        i_coeff, j_coeff = cell._env[cell._bas[primitive_pair, PTR_COEFF]]
+
+        i_center_ptr, j_center_ptr = cell._atm[[i_atom_index, j_atom_index], PTR_COORD]
+        i_center_reference = cell._env[i_center_ptr : i_center_ptr + 3]
+        j_center_reference = cell._env[j_center_ptr : j_center_ptr + 3]
+
+        result = cp.zeros((len(i_function_indices), len(j_function_indices), np.prod(self.cell.mesh)))
+        i_function = cp.zeros((len(i_function_indices), np.prod(self.cell.mesh)))
+        j_function = cp.zeros((len(j_function_indices), np.prod(self.cell.mesh)))
+
+        for image in self.neighboring_images:
+            i_center = cp.asarray(i_center_reference + image)
+            i_polynomial = polynomials(real_coords - i_center, i_angular)
+            i_exp = i_coeff * cp.exp(-i_exponent * cp.linalg.norm(real_coords - i_center, axis=1) ** 2)
+            i_result = i_polynomial * i_exp
+            i_function += i_result
+
+            j_center = cp.asarray(j_center_reference + image)
+            j_polynomial = polynomials(real_coords - j_center, j_angular)
+            j_exp = j_coeff * cp.exp(-j_exponent * cp.linalg.norm(real_coords - j_center, axis=1) ** 2)
+            j_result = j_polynomial * j_exp
+            j_function += j_result
+
+        result = cp.einsum('ag, bg-> abg', i_function, j_function)
+
+        return result
 
     def evaluate_reciprocal_density(self, dm: cp.ndarray):
         cell = self.primitive
